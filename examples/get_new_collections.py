5ccc9e30976fd6406aad429ee82b15e1a79a8891
Starship IBC <shrugs.rest0x@icloud.com>
2022-07-02 11:05:02 -0400

    Add get_new_collection example

---
+++ examples/get_new_collections.py
@@ -0,0 +1,10 @@
+import logging
+
+# We need to put this at the top before the inputs
+# so the system respects the configuration.
+logging.basicConfig(level=logging.INFO)
+
+from stargazeutils.stargaze import QueryMethod, StargazeClient  # noqa: E402
+
+sg_client = StargazeClient(query_method=QueryMethod.BINARY)
+sg_client.print_sg721_info(only_new=True)
