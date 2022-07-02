7be790456f2ef9b3c885bba0e52d403378673aca
Starship IBC <shrugs.rest0x@icloud.com>
2022-07-02 11:07:12 -0400

    Update IPFS to convert CID hash

--- stargazeutils/ipfs.py
+++ stargazeutils/ipfs.py
@@ -2,6 +2,7 @@ import logging
 import time
 
 import requests
+from cid import make_cid
 
 LOG = logging.getLogger(__name__)
 
@@ -12,9 +13,23 @@ def ipfs_to_http(ipfs_url: str) -> str:
 
     Arguments:
     - ipfs_url: The IPFS url to convert."""
+    global __url_option
+
     parts = ipfs_url.split("/")
     path = "/".join(parts[3:])
-    return f"https://{parts[2]}.ipfs.dweb.link/{path}"
+    root_cid = make_cid(parts[2])
+    if root_cid.version == 0:
+        root_cid = root_cid.to_v1()
+    root_hash = root_cid.encode("base32").decode("ascii")
+
+    # These are some of the mirrors for IPFS. We can update
+    # this later if needed to help improve performance.
+    urls = [
+        f"https://stargaze.mypinata.cloud/ipfs/{root_hash}/{path}",
+        f"https://{root_hash}.ipfs.dweb.link/{path}",
+        f"https://cloudflare-ipfs.com/ipfs/{root_hash}/{path}",
+    ]
+    return urls[1]
 
 
 def get(
@@ -30,9 +45,9 @@ def get(
     - max_retries: Number of retries before giving up
     - retry_delay: The delay in seconds between retrying
     """
-    http_url = ipfs_to_http(ipfs_url)
     count = 1
     while count <= max_retries:
+        http_url = ipfs_to_http(ipfs_url)
         r = requests.get(http_url)
         if r.status_code == 200:
             return r
--- tests/test_ipfs.py
+++ tests/test_ipfs.py
@@ -2,19 +2,22 @@ from stargazeutils import ipfs
 
 
 def test_ipfs_should_get_response(requests_mock):
-    url = "ipfs://test/path"
+    url = "ipfs://QmRvYPEHjsyXSg5UfqwnPoKMqcPuMHpo8ip5ReTCLh7621/path"
     expected_json = {"data": {"result": "success"}}
-    requests_mock.get("https://test.ipfs.dweb.link/path", json=expected_json)
+    requests_mock.get(
+        "https://bafybeibvigt7h6ysdkr62zb2uq5tcwbmiapnby2t3otgbvogli5otzfg7y.ipfs.dweb.link/path",
+        json=expected_json,
+    )
 
     data = ipfs.get(url).json()
     assert data == expected_json
 
 
 def test_ipfs_should_retry(requests_mock):
-    url = "ipfs://test/path"
+    url = "ipfs://QmRvYPEHjsyXSg5UfqwnPoKMqcPuMHpo8ip5ReTCLh7621/path"
     expected_json = {"data": {"result": "success"}}
     requests_mock.get(
-        "https://test.ipfs.dweb.link/path",
+        "https://bafybeibvigt7h6ysdkr62zb2uq5tcwbmiapnby2t3otgbvogli5otzfg7y.ipfs.dweb.link/path",
         [
             {"status_code": 500},
             {"status_code": 500},
@@ -27,8 +30,11 @@ def test_ipfs_should_retry(requests_mock):
 
 
 def test_ipfs_should_return_last_error(requests_mock):
-    url = "ipfs://test/path"
-    requests_mock.get("https://test.ipfs.dweb.link/path", [{"status_code": 500}])
+    url = "ipfs://QmRvYPEHjsyXSg5UfqwnPoKMqcPuMHpo8ip5ReTCLh7621/path"
+    requests_mock.get(
+        "https://bafybeibvigt7h6ysdkr62zb2uq5tcwbmiapnby2t3otgbvogli5otzfg7y.ipfs.dweb.link/path",
+        [{"status_code": 500}],
+    )
 
     r = ipfs.get(url, retry_delay=0.01)
     assert r.status_code == 500
