5a2f163c7a240227de2925654e4e70e564f24410
Starship IBC <shrugs.rest0x@icloud.com>
2022-07-04 22:03:51 -0400

    Include trait rarity in collection info example

--- examples/get_collection_info.py
+++ examples/get_collection_info.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 
 import requests_cache
@@ -14,14 +15,27 @@ from stargazeutils.stargaze import QueryMethod, StargazeClient  # noqa: E402
 requests_cache.install_cache("stargaze-ipfs")
 
 sg_client = StargazeClient(query_method=QueryMethod.BINARY)
-sg_client.print_sg721_info(only_new=True)
+# sg_client.print_sg721_info(only_new=True)
+
+if len(sys.argv) < 2:
+    print("No collection specified.")
+    exit(0)
 
 collection_name = sys.argv[1]
 
-info = sg_client._sg721_cache.get_sg721_info_from_name(collection_name)
-client = Sg721Client(info.sg721, info.minter, sg_client)
+client = Sg721Client.from_collection_name(collection_name, sg_client)
 
+print("")
 client.query_collection_info().print()
+print("")
 client.query_minter_config().print()
 
-# client.fetch_traits()
+cache_dir = os.path.join(os.curdir, "cache", "collections")
+json_file = collection_name.replace(" ", "-") + ".json"
+json_trait_cache_file = os.path.join(cache_dir, json_file)
+
+traits = client.fetch_nft_collection(json_trait_cache_file)
+traits.export_json(json_trait_cache_file)
+
+print("")
+traits.print_trait_rarity()
