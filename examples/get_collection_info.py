de6ad18386dc8b32f0d08863df01e3d6559f6529
Starship IBC <shrugs.rest0x@icloud.com>
2022-07-08 23:13:32 -0400

    Add doc info

--- examples/get_collection_info.py
+++ examples/get_collection_info.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+#
+# This script fetches information for a given collection
+# and then begins fetching all token metadata from IPFS.
+# The initial fetch can take a while so if you want more
+# detailed feedback, set the logging level to DEBUG.
+#
+# Once the metadata has been fetched, it's exported to
+# JSON file in the "cache/collections" directory so if
+# the script is run again, you will notice a distinct
+# increase in speed.
+#
+# Usage:
+#   poetry run python3 examples/get_collection_info.py "<collection_name>"
+
 import logging
 import os
 import sys
@@ -25,13 +40,17 @@ collection_name = sys.argv[1]
 
 client = Sg721Client.from_collection_name(collection_name, sg_client)
 
+if client is None:
+    print(f"Collection '{collection_name}' not found.")
+    exit(1)
+
 print("")
 client.query_collection_info().print()
 print("")
 client.query_minter_config().print()
 
 cache_dir = os.path.join(os.curdir, "cache", "collections")
-json_file = collection_name.replace(" ", "-") + ".json"
+json_file = collection_name.lower().replace(" ", "-") + ".json"
 json_trait_cache_file = os.path.join(cache_dir, json_file)
 
 traits = client.fetch_nft_collection(json_trait_cache_file)
--- examples/get_new_collections.py
+++ examples/get_new_collections.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+#
+# This script prints out a list of new collections
+# since the last time this script was run. It also
+# automatically updates the local cache file that
+# holds the SG721 and minter information for each
+# collection.
+#
+# Usage:
+#  poetry run python3 examples/get_new_collections.py
+
 import logging
 
 # We need to put this at the top before the inputs
